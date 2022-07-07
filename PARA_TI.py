8b72352c80dcf63d423a4cf8e937b62d8ccf11eb
97hackbrian <44384415+97hackbrian@users.noreply.github.com>
2022-07-07 19:56:06 -0400

    mainV1_1

--- PARA_TI.py
+++ PARA_TI.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#Cuando una personsa quiere mucho a otra
+#Su corazón se encarga de hacerle saber que
+#Es ahí
 ########################## MAIN ###################################
 from tkinter import *
 from tkinter import ttk
@@ -12,52 +16,22 @@ c=0
 
 xa2=0
 ya2=0
-
-#x1=int(random.randint(0,500))
-#y1=int(random.randint(0,600))
-# La clase 'Aplicacion' ha crecido. En el ejemplo se incluyen
-# nuevos widgets en el método constructor __init__(): Uno de
-# ellos es el botón 'Info'  que cuando sea presionado llamará 
-# al método 'verinfo' para mostrar información en el otro 
-# widget, una caja de texto: un evento ejecuta una acción: 
 class Aplicacion():
-    
-    
-    
-    
-    
-    
     def __init__(self):
-
-    
-        # En el ejemplo se utiliza el prefijo 'self' para
-        # declarar algunas variables asociadas al objeto 
-        # ('mi_app')  de la clase 'Aplicacion'. Su uso es 
-        # imprescindible para que se pueda acceder a sus
-        # valores desde otros métodos:
         self.one = Tk()
         self.one.geometry('700x500')
         self.one.configure(background = 'pink')
         style1 = ttk.Style()
         style1.configure('W.TButton', font =('calibri', 15, 'bold'),foreground = 'red')
         
-        # Impide que los bordes puedan desplazarse para
-        # ampliar o reducir el tamaño de la ventana 'self.raiz':
         self.one.resizable(width=False,height=False)
         self.one.title("PRINCESA")
-        #fontTextHis = tkFont.Font(family="Comic Sans MS", size=14, weight="bold",slant="italic")
-        #parraFo="La historia cuenta que.... \n Una vez dos personas se se vieron sin imaginarse que algún día \n llegarian a estar juntos......"
-        #self.historia=Label(self.one, text=parraFo,fg='black', bg = 'pink')
-        #self.historia.configure(font=fontTextHis)
-        #self.historia.pack(side=TOP)
+
         self.next=Button(self.one,text="next",command=self.loqueSeaXD)
         self.next.place(x=350,y=400)
-        #self.binfo.focus_set()
-        
+
         self.one.mainloop()
         self.wind2.mainloop()
-        
-    
         
     def loqueSeaXD(self):
         global c
@@ -84,22 +58,15 @@ class Aplicacion():
             self.next=Button(self.one,text="next",command=self.Window2)
             self.next.place(x=350,y=400)
         
-        
-
-        
     def Window2(self):
         self.wind2=Tk()
         self.wind2.geometry('700x500')
         self.wind2.configure(background='pink')
-        fontTextHis = tkFont.Font(family="Comic Sans MS", size=14, weight="bold",slant="italic")
-        
-        parraFo="hola 2"
+        parraFo="Quizás no sea la persona\nperfecta para ti, cometo errores,\ntengo miles de defectos; pero.\nhago lo posible por enamorarte,\npor hacerte feliz, día a día me\nesfuerzo por darte lo mejor de mi\npara que te sientas bien y que\nseas la persona más feliz del\nmundo junto a mi."
         self.historia2=Label(self.wind2, text=parraFo,fg='black', bg = 'pink')
-        self.historia2.configure(font=fontTextHis)
+        self.historia2.configure(font=('Comic Sans MS', 16, 'bold'))
         self.historia2.pack(side=TOP)
-        
-        
-        self.next=Button(self.wind2,text="next2",command=self.WindowMain)
+        self.next=Button(self.wind2,text="next22",command=self.WindowMain)
         self.next.place(x=350,y=400)
         self.wind2.mainloop()
 
@@ -112,84 +79,40 @@ class Aplicacion():
         style3 = ttk.Style()
         style1.configure('W.TButton', font =('calibri', 15, 'bold'),foreground = 'red')
         style2.configure('W2.TButton', font =('calibri', 15, 'bold'),foreground = 'blue')
-        
-        # Impide que los bordes puedan desplazarse para
-        # ampliar o reducir el tamaño de la ventana 'self.raiz':
         self.raiz.resizable(width=False,height=False)
         self.raiz.title("PERSEVERANCE")
-
-
-
         self.ques=Label(self.raiz, text="¿Quieres ser mi Novia?", font=("Bradley Hand ITC", 30, 'bold'),fg='blue', bg = 'pink')
         self.ques.pack(side=TOP)
-        self.xa = IntVar(value=1)#VALOR DE VARIBLE AUXILIAR PARA EL BOTON
-        #self.posx= IntVar(value=x1)
-        #self.posy= IntVar(value=y1)
-        # Define el widget Text 'self.tinfo ' en el que se
-        # pueden introducir varias líneas de texto:
+        self.xa = IntVar(value=1)
 
         style3.configure('W3.TButton', font =('Arial', 15, 'bold'),foreground = 'black', background = 'pink')
         
-        self.tinfo = Text(self.raiz,bg='pink', width=60, height=3)#40 10 
-        
-        # Sitúa la caja de texto 'self.tinfo' en la parte
-        # superior de la ventana 'self.raiz':
-        
-        self.tinfo.pack(side=BOTTOM)
-        
-        # Define el widget Button 'self.binfo' que llamará 
-        # al metodo 'self.verinfo' cuando sea presionado
-        
-        self.binfo = ttk.Button(self.raiz, text='SI', style='W2.TButton', command=self.createNewWindow)
-        
-        # Coloca el botón 'self.binfo' debajo y a la izquierda
-        # del widget anterior
-                                
-        self.binfo.pack(side=LEFT)
-        
-        # Define el botón 'self.bsalir'. En este caso
-        # cuando sea presionado, el método destruirá o
-        # terminará la aplicación-ventana 'self.raíz' con 
-        # 'self.raiz.destroy'
-        
+        self.tinfo = Text(self.raiz,bg='pink', width=60, height=3)      
+        self.tinfo.pack(side=BOTTOM)      
+        self.binfo = ttk.Button(self.raiz, text='SI', style='W2.TButton', command=self.createNewWindow)                    
+        self.binfo.pack(side=LEFT)      
         self.bsalir = ttk.Button(self.raiz, text='NO',style = 'W.TButton', command=self.nada)
-
-        #self.bsalir.grid(row = 2, column = 2, sticky = W)
-
-        #self.bsalir.configure(self.raiz, bg = "blue")
-        #self.bsalir.pack()
-                                  #command=self.raiz.destroy)
-                                 
-        # Coloca el botón 'self.bsalir' a la derecha del 
-        # objeto anterior.
         self.bsalir.pack(side=RIGHT)
         self.bsalir.bind("<Enter>",self.enter)
         self.raiz.mainloop()
-
+        
     def salir(self):
         self.raiz.destroy()
         self.wind2.destroy()
         self.one.destroy()
-
-      
+        
     def createNewWindow(self):
         top = Toplevel() 
         top.geometry("650x230") 
         top.title("¡SI!") 
         l2 = Label(top, text = "Sabia que dirias que si mi princesa ( <3 ) TE QUIERO ∞", font =('Brush Script MT', 17, 'bold'))
-        #ttk().self.l2.config(font =('Arial', 11, 'bold'),foreground = 'black', background = 'pink')
-        
         l2.pack()
         bsalir2 = ttk.Button(top, text='SALIR', command=self.salir)
-                                  #command=self.raiz.destroy)
         bsalir2.pack(side=BOTTOM)
-        top.mainloop() 
-    
-    
-
+        top.mainloop()
+        
     def nada(self):
         print("nada")
-
         
     def enter(self, *args):
         
@@ -210,17 +133,8 @@ class Aplicacion():
                 print("se hace uso")
             xa2=x2
             ya2=y2
-        #if(x==1):
-            #x2=int(random.randrange(380,500,7))
-            #y2=int(random.randrange(200,380,7))
-            #self.bsalir.place(x=x2,y=y2)
-        #if(x==2):
-            ##x2=int(random.randrange(380,500,7))
-            ##y2=int(random.randrange(200,380,5))
-            #self.bsalir.place(x=x2+20,y=y2)
+
         if(x<7):
-            ##x2=int(random.randrange(380,500,3))
-            ##y2=int(random.randrange(200,380,3))
             self.bsalir.place(x=x2+25,y=y2)
         if(x>=7):
             x1=int(random.randrange(0,500,99))
@@ -232,16 +146,11 @@ class Aplicacion():
             print("X1 -> ",x1)
             print("Y1 -> ",y1)
             self.bsalir.place(x=x1,y=y1)
-             #self.xa.set(1)
             xa1=x1
             ya1=y1
         self.verinfo()
-
         
-    def verinfo(self):    
-        # Borra el contenido que tenga en un momento dado
-        # la caja de texto
-        
+    def verinfo(self):
         self.tinfo.delete("1.0", END)
         self.tinfo.config(font =('Arial', 11, 'bold'),foreground = 'black', background = 'pink')
         x = self.xa.get()
@@ -258,15 +167,12 @@ class Aplicacion():
         elif(x==7):
             texto_info = "jajajaa sigue intentandoo -te pasas - Muah! - Te quiero mucho - Muah!"
         elif(x>=8):
-            texto_info = "¿No puedes decir no?  jajajajjajajjajajajajajajjajajajaa  <3 "    
+            texto_info = "¿No puedes decir no?  jajajajajajajajajajajajajajajaa  <3 "
+        self.tinfo.insert("2.0", texto_info)
         
-        self.tinfo.insert("1.0", texto_info)
-
 def main():
     mi_app = Aplicacion()
-    
-    
-    return 0
 
+    return 0
 if __name__ == '__main__':
     main()
